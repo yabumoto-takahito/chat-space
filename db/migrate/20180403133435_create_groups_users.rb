05fde18c2774251994c0f21fbdb114001ae877a4
yabumoto-takahito <yabumototakahito@yabumototakahitonoMacBook-puro.local>
2018-04-03 22:57:03 +0900

    Edit migration-fail of groups_users
    groupカラム、userカラムを作成する為にマイグレーションファイルを編集。

--- db/migrate/20180403133435_create_groups_users.rb
+++ db/migrate/20180403133435_create_groups_users.rb
@@ -1,7 +1,8 @@
 class CreateGroupsUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :groups_users do |t|
-
+      t.references :user, null: false,foreign_key: true
+      t.references :group, null: false, foreign_key: true
       t.timestamps
     end
   end
