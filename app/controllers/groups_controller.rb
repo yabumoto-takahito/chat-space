75307c83553d82d40b6eb28789ebb2535ab0808c
yabumoto-takahito <yabumototakahito@yabumototakahitonoMacBook-puro.local>
2018-04-06 23:21:26 +0900

    Edit groups_controller.rb
    createアクション内に、グループ作成の可否にあわせた処理を記述。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,5 +11,16 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, {:user_ids => []})
   end
 end
+
