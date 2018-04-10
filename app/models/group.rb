cdb8bae40995f33959726fadd26360c63e6554ed
yabumoto-takahito <yabumototakahito@yabumototakahitonoMacBook-puro.local>
2018-04-10 23:43:20 +0900

    Revert "Define association"
    This reverts commit 77199bd6aa7c78dbb00bae9858c41e9a026282e0.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,4 @@
 class Group < ApplicationRecord
-  has_many :messages
   has_many :groups_users
   has_many :users, through: :groups_users
   validates :name, presence: true
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,2 @@
 class Message < ApplicationRecord
-  belongs_to :group
-  belongs_to :user
 end
