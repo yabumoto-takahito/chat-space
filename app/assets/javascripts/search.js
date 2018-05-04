a18c89fcd53ea07426edf8c745afadd846948474
yabumoto-takahito <yabumototakahito@yabumototakahitonoMacBook-puro.local>
2018-05-04 11:46:07 +0900

    Edit search.js
    非同期通信の結果をdoneの関数の引数から受取り、ビューに追加するためのHTMLを作成。

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,14 @@
 $(function(){
+  var search_list = $('#user-search-result');
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">
+                    ${user.name}
+                  </p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}"追加</a>
+                </div>`
+    search_list.append(html);
+  }
   $('#user-search-field').on('keyup', function(){
     var input = $('#user-search-field').val();
     $.ajax({
@@ -15,7 +25,7 @@ $(function(){
         });
       }
       else {
-        appendNoProduct("一致するユーザーはいません");
+        appendNoUser("一致するユーザーはいません");
       }
     })
     .fail(function(){
