8db91a485146f2d5a4312f971467a6fe5ce6ef90
yabumoto-takahito <yabumototakahito@yabumototakahitonoMacBook-puro.local>
2018-05-07 18:42:38 +0900

    Fix views
    ・_form.html.haml
    ・search.js
    を編集してビューを修正。

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,18 +3,14 @@ $(function(){
   var user_list = $('#chat-group-users');
   function appendUser(user){
     var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">
-                    ${user.name}
-                  </p>
+                  <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
     search_list.append(html);
   }
   function appendNoUser(user){
     var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">
-                    ${user}
-                  </p>
+                  <p class="chat-group-user__name">${user}</p>
                 </div>`
     search_list.append(html);
   }
